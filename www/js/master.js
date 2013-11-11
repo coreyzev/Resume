8487168fd91d803145d38e36091f80d438b3d774
coreyzev <coreyzev@zevdesigns.com>
2013-11-11 16:17:03 -0500

    add photo heading section

--- www/js/master.js
+++ www/js/master.js
@@ -1,18 +1,23 @@
 $(document).ready(function() {
 
   // Smooth Scroll
-  $('a[href*=#]:not([href=#])').click(function() {
-    if (location.pathname.replace(/^\//,'') == this.pathname.replace(/^\//,'') && location.hostname == this.hostname) {
-      var target = $(this.hash);
-      target = target.length ? target : $('[name=' + this.hash.slice(1) +']');
-      if (target.length) {
-        $('html,body').animate({
-          scrollTop: target.offset().top
-        }, 500);
-        return false;
-      }
-    }
+  $(function() {
+    $('a[href*=#]:not([href=#])').click(function() {
+      if (location.pathname.replace(/^\//,'') == this.pathname.replace(/^\//,'') 
+          || location.hostname == this.hostname) {
+
+          var target = $(this.hash);
+          target = target.length ? target : $('[name=' + this.hash.slice(1) +']');
+             if (target.length) {
+               $('html,body').animate({
+                   scrollTop: target.offset().top
+              }, 500);
+              return false;
+            }
+        }
+    })
   });
+
 
   // Initialize Tooltips
   $('[data-toggle=tooltip]').tooltip();
