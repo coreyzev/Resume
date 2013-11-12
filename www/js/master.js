2b997e3b3bc9f3d1790a8a60ac04e1cae16f6fd3
Corey Zev <coreyzev@zevdesigns.com>
2013-11-12 04:45:46 -0500

    added details & functioning scrollspy

--- www/js/master.js
+++ www/js/master.js
@@ -1,175 +1,186 @@
 $(document).ready(function() {
 
-  // Smooth Scroll
-  $(function() {
-    $('a[href*=#]:not([href=#])').click(function() {
-      if (location.pathname.replace(/^\//,'') == this.pathname.replace(/^\//,'') 
-        || location.hostname == this.hostname) {
+    // Smooth Scroll
+    $(function() {
+        $('a[href*=#]:not([href=#])').click(function() {
+            if (location.pathname.replace(/^\//, '') == this.pathname.replace(/^\//, '') || location.hostname == this.hostname) {
 
-        var target = $(this.hash);
-      target = target.length ? target : $('[name=' + this.hash.slice(1) +']');
-      if (target.length) {
-       $('html,body').animate({
-         scrollTop: target.offset().top
-       }, 500);
-       return false;
-     }
-   }
- })
-  });
+                var target = $(this.hash);
+                target = target.length ? target : $('[name=' + this.hash.slice(1) + ']');
+                if (target.length) {
+                    $('html,body').animate({
+                        scrollTop: target.offset().top
+                    }, 500);
+                    return false;
+                }
+            }
+        })
+    });
+
+    //ScrolSpy
+
+    //Sliding Nav Active Highlight
+    $('body').on('activate.bs.scrollspy', function(e) {
+        var a = e.target.childNodes[0].hash;
+        var ln = '.sidebar-offcanvas [href=' + a + ']';
+        $('.sidebar-offcanvas a').each(function() {
+            var $this = $(this);
+            if (this.hash == a) {
+                $this.addClass('active');
+            } else {
+                $this.removeClass('active');
+            };
+        });
+    });
 
 
-  // Initialize Tooltips
-  $('[data-toggle=tooltip]').tooltip();
+    // Initialize Tooltips
+    $('[data-toggle=tooltip]').tooltip();
 
-  // Sliding Navigation
-  $('[data-toggle=offcanvas]').click(function() {
-    $('.row-offcanvas').toggleClass('active');
-    $('#overlay').toggleClass('active');
-    $('[data-toggle=offcanvas]').blur();
-  });
+    // Sliding Navigation
+    $('[data-toggle=offcanvas]').click(function() {
+        $('.row-offcanvas').toggleClass('active');
+        $('#overlay').toggleClass('active');
+        $('[data-toggle=offcanvas]').blur();
+    });
 
-  $('.sidebar-offcanvas a, #overlay').click(function() {
-    $('.row-offcanvas').removeClass('active');
-    $('#overlay').toggleClass('active');
-  });
+    $('.sidebar-offcanvas a, #overlay').click(function() {
+        $('.row-offcanvas').removeClass('active');
+        $('#overlay').toggleClass('active');
+    });
 
-  // Updated Time
-  lastmod = document.lastModified;     // get string of last modified date
-  lastmoddate = Date.parse(lastmod);   // convert modified string to date
-  modMoment = moment.utc(lastmod, "MM-DD-YYYY HH:mm:ss").subtract('second',70).fromNow();
-  if (lastmoddate == 0) {               // unknown date (or January 1, 1970 GMT)
-    document.writeln("Last Modified: Unknown")
-  } else {
-   $('#momentupdate').html(modMoment);
- };
+    // Updated Time
+    lastmod = document.lastModified; // get string of last modified date
+    lastmoddate = Date.parse(lastmod); // convert modified string to date
+    modMoment = moment.utc(lastmod, "MM-DD-YYYY HH:mm:ss").subtract('second', 70).fromNow();
+    if (lastmoddate == 0) { // unknown date (or January 1, 1970 GMT)
+        document.writeln("Last Modified: Unknown")
+    } else {
+        $('#momentupdate').html(modMoment);
+    };
 
-  // Resume Question Toggle
-  var alerts = $('#resumePrompt .alert');
-  $(alerts[1]).hide();
-  $(alerts[2]).hide();
-  $('#resumePrompt .alert-warning span').hide();
-  $('#resumePrompt .alert.first').show();
-  $('#resumePrompt .btn-warning').mouseover(function() {
-    $(alerts).hide();
-    $(alerts[1]).show();
-  }).on('click', function(){
-    $('#resumePrompt .alert-warning span').show('slow');
-  });
-  $('#resumePrompt .btn-primary').mousedown(function() {
-    $(alerts).hide();
-    $(alerts[2]).show();
+    // Resume Question Toggle
+    var alerts = $('#resumePrompt .alert');
+    $(alerts[1]).hide();
+    $(alerts[2]).hide();
     $('#resumePrompt .alert-warning span').hide();
-  });
-
-  $('#resumePrompt .btn-group').click(function(){
-    $('#resumePrompt .btn-group .btn').toggleClass('disabled');
-    $('.resumeHide').toggleClass('hidden');
-  });
-
-
-  $('#expPills a').click(function (e) {
-    e.preventDefault()
-    $(this).tab('show')
-  });
-
-  $( '#expSelect' )
-  .change(function() {
-    $( "select option:selected" ).each(function() {
-      var str = "";
-      var targ = $(this).attr('data-target');
-      str = '#expPills a[href="' + targ + '"]'
-      $(str).click();
+    $('#resumePrompt .alert.first').show();
+    $('#resumePrompt .btn-warning').mouseover(function() {
+        $(alerts).hide();
+        $(alerts[1]).show();
+    }).on('click', function() {
+        $('#resumePrompt .alert-warning span').show('slow');
     });
-  })
-  .trigger( "change" );
+    $('#resumePrompt .btn-primary').mousedown(function() {
+        $(alerts).hide();
+        $(alerts[2]).show();
+        $('#resumePrompt .alert-warning span').hide();
+    });
 
-  // Tag Cloud
-  $.fn.tagcloud.defaults = {
-    size: {start: 10, end: 14, unit: 'px'},
-    color: {start: '#CDDFEE', end: '#8299AB'}
-  };
+    $('#resumePrompt .btn-group').click(function() {
+        $('#resumePrompt .btn-group .btn').toggleClass('disabled');
+        $('.resumeHide').toggleClass('hidden');
+    });
 
-  $(function () {
-    $('#skills #tagcloud a').tagcloud().css('padding-right', '5px');;
-  });
 
-  // Fancybox
-  var images = {
-    1: [
-    {
-      href : 'img/me.png',                
-      title : 'I still think I look good here.'
-    },
-    {
-      href : 'img/cz_knit.jpg',                
-      title : 'Self Portrait. Sinister.'
-    },
-    {
-      href : 'img/cz_link.jpg',                
-      title : 'Hey Listen!'
-    },
-    {
-      href : 'img/cz_fire.jpg',                
-      title : "I'm a hot commodity."
-    },
-    {
-      href : 'img/cz_mom.jpg',                
-      title : 'Family commitment'
-    },
-    {
-      href : 'img/cz_apples.jpg',                
-      title : "I can't wait to be this happy to go to work."
-    },
-    {
-      href : 'img/pants-rip.gif',                
-      title : "I can't wait to be this happy to go to work."
-    }
-    ],
-    2: [
-    {
-      href : 'http://www.zevdesigns.com/content/Corey_Zev_Holland-resume.pdf',                
-      title : 'Resume'
-    },
-    {
-      href : 'http://fancyapps.com/fancybox/demo/5_b.jpg',                
-      title : 'Gallery 2 - 2'
-    }
-    ]
-  };
+    $('#expPills a').click(function(e) {
+        e.preventDefault()
+        $(this).tab('show')
+    });
 
-  $("#profphoto").mouseup(function() {
+    $('#expSelect')
+        .change(function() {
+            $("select option:selected").each(function() {
+                var str = "";
+                var targ = $(this).attr('data-target');
+                str = '#expPills a[href="' + targ + '"]'
+                $(str).click();
+            });
+        })
+        .trigger("change");
 
-    $.fancybox.open(images[1], {
-      nextEffect : 'none',
-      prevEffect : 'none',
-      loop       : false,
-      nextClick  : true,
-      padding    : 0,
-      helpers    : {
-        thumbs : {
-          width  : 50,
-          height : 50
+    // Tag Cloud
+    $.fn.tagcloud.defaults = {
+        size: {
+            start: 0.9,
+            end: 1.15,
+            unit: 'em'
+        },
+        color: {
+            start: '#AFC3D5',
+            end: '#8299AB'
         }
-      }
+    };
+
+    $(function() {
+        $('#skills #tagcloud a').tagcloud().css('padding-right', '5px');;
     });
 
-    return false;
-  });
+    // Fancybox
+    var images = {
+        1: [{
+            href: 'img/me.png',
+            title: 'I still think I look good here.'
+        }, {
+            href: 'img/cz_knit.jpg',
+            title: 'Self Portrait. Sinister.'
+        }, {
+            href: 'img/cz_link.jpg',
+            title: 'Hey Listen!'
+        }, {
+            href: 'img/cz_fire.jpg',
+            title: "I'm a hot commodity."
+        }, {
+            href: 'img/cz_mom.jpg',
+            title: 'Family commitment'
+        }, {
+            href: 'img/cz_apples.jpg',
+            title: "I can't wait to be this happy to go to work."
+        }, {
+            href: 'img/pants-rip.gif',
+            title: "I can't wait to be this happy to go to work."
+        }],
+        2: [{
+            href: 'http://www.zevdesigns.com/content/Corey_Zev_Holland-resume.pdf',
+            title: 'Resume'
+        }, {
+            href: 'http://fancyapps.com/fancybox/demo/5_b.jpg',
+            title: 'Gallery 2 - 2'
+        }]
+    };
 
-  $(".openPDF").fancybox({
-    openEffect  : 'none',
-    closeEffect : 'none',
-    iframe : {
-        preload: false
-    },
-    afterLoad: function() {
-        this.title = '<a class="btn btn-success" href="' + this.href + '">Download &raquo;</a> ' + this.title;
-    },
-    wrapCSS : 'PDFviewer',
-    fitToView : true,
-    height : '100%',
-    padding : 0
-});
+    $("#profphoto").mouseup(function() {
+
+        $.fancybox.open(images[1], {
+            nextEffect: 'none',
+            prevEffect: 'none',
+            loop: false,
+            nextClick: true,
+            padding: 0,
+            helpers: {
+                thumbs: {
+                    width: 50,
+                    height: 50
+                }
+            }
+        });
+
+        return false;
+    });
+
+    $(".openPDF").fancybox({
+        openEffect: 'none',
+        closeEffect: 'none',
+        iframe: {
+            preload: false
+        },
+        afterLoad: function() {
+            this.title = '<a class="btn btn-success" href="' + this.href + '">Download &raquo;</a> ' + this.title;
+        },
+        wrapCSS: 'PDFviewer',
+        fitToView: true,
+        height: '100%',
+        padding: 0
+    });
 
 });
